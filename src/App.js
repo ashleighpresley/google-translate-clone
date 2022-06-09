ad50db6a9d98cd89a9e77f828c9c5b7c5eaec98c
ashleighpresley <ashleighpresley1@gmail.com>
2022-06-09 16:18:45 -0500

    Using axios to pull in languages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import Arrows from "./components/Arrows";
 import Button from "./components/Button";
@@ -9,13 +10,42 @@ function App() {
   const [showModal, setShowModal] = useState(null);
   const [inputLanguage, setInputLanguage] = useState("English");
   const [outputLanguage, setOutputLanguage] = useState("Polish");
+  const [languages, setLanguages] = useState(null);
+
+  const getLanguages = () => {
+    const options = {
+      method: "GET",
+      url: "https://google-translate20.p.rapidapi.com/languages",
+      headers: {
+        "X-RapidAPI-Key": "dbcc677bc6msh31df28e8d9fe4e9p1065c4jsne25d8f6feb56",
+        "X-RapidAPI-Host": "google-translate20.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        const arrayOfData = Object.keys(response.data.data).map(
+          (key) => response.data.data[key]
+        );
+        setLanguages(arrayOfData);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  console.log("languages", languages);
+
+  useEffect(() => {
+    getLanguages();
+  }, []);
 
   const handleClick = () => {
     setInputLanguage(outputLanguage);
     setOutputLanguage(inputLanguage);
   };
-
-  console.log("showModal", showModal);
 
   return (
     <div className="app">
@@ -36,7 +66,7 @@ function App() {
           />
         </>
       )}
-      {showModal && <Modal setShowModal={setShowModal} />}
+      {showModal && <Modal setShowModal={setShowModal} languages={languages} />}
     </div>
   );
 }
