550ae5b279dadef9a3ef44f390255a8a695e438d
ashleighpresley <ashleighpresley1@gmail.com>
2022-06-09 13:55:04 -0500

    Adding input and output textareas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Arrows from "./components/Arrows";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
+import TextBox from "./components/TextBox";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <TextBox style={`input`} />
+      <TextBox style={`output`} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,5 +1,12 @@
-function TextBox() {
-  return <div>TextBox</div>;
+function TextBox({ style }) {
+  return (
+    <div>
+      <textarea
+        placeholder={style === "input" ? "Enter Text" : "Translation"}
+        disabled={style === "output"}
+      ></textarea>
+    </div>
+  );
 }
 
 export default TextBox;
